04e486e53b7578d21d3ef43a7a7b81e34f170e12
Jaya Vunna <you@example.com>
2019-02-11 20:02:29 +0000

    eeeogewogg

---
+++ src/loveProgramming/AutoBoxing.java
@@ -0,0 +1,21 @@
+package loveProgramming;
+
+import java.util.ArrayList;
+
+public class AutoBoxing {
+    public static void main(String[] args)
+    {
+        char ch = 'a';
+
+        // Autoboxing- primitive to Character object conversion
+        Character a = ch;
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        // Autoboxing because ArrayList stores only objects
+        arrayList.add(25);
+
+        // printing the values from object
+        System.out.println(arrayList.get(0));
+    }
+}
